922148dcb9491a9db3b163ca4f8e51e6ca9edb24
Matthew Wetstein <mwetstein@Matthews-MacBook-Air-3.local>
2014-12-01 08:59:15 -0600

    added lots of comments, took out unecessary variables

--- src/LAPPDresponse.cc
+++ src/LAPPDresponse.cc
@@ -15,17 +15,22 @@ ClassImp(LAPPDresponse)
 LAPPDresponse::LAPPDresponse()
 {
   TFile* tf = new TFile("pulsecharacteristics.root","READ");
- 
-  _templatepulse = (TH1D*) tf->Get("templatepulse");
-  _PHD = (TH1D*) tf->Get("PHD");
-  _pulsewidth = (TH1D*) tf->Get("pulsewidth");
-  
-  _noiselevel = 1.0; //in mV
-  _nsamples = 35;
-  _samplewidth = 100; //in picoseconds
 
+  // the shape of a typical pulse 
+  _templatepulse = (TH1D*) tf->Get("templatepulse");
+  // variations in the peak signal on the central strip
+  _PHD = (TH1D*) tf->Get("PHD");
+
+  // charge spreading of a pulse in the transverse direction (in mm)
+  // as a function of nearness to strip center. The charge tends to 
+  // spread more in the transverse direction if the centroid of the
+  // signal is between two striplines
+  _pulsewidth = (TH1D*) tf->Get("pulsewidth");
+
+  // structure to store the pulses, count them, and organize them by channel
   _pulseCluster = new LAPPDpulseCluster();
 
+  // random numbers for generating noise
   mrand = new TRandom3();
 }
 
@@ -92,8 +97,10 @@ std::cout<<"Done Adding Pulse"<<std::endl;
 }
 
 	
-TH1D* LAPPDresponse::GetTrace(int CHnumber, int parity, double starttime, double samplesize, int numsamples)
+TH1D* LAPPDresponse::GetTrace(int CHnumber, int parity, double starttime, double samplesize, int numsamples, double thenoise)
 {
+
+  // parameters for the histogram of the scope trace
   double lowend = (starttime-(samplesize/2.));
   double upend = lowend + samplesize*((double)numsamples);
   TString tracename;
@@ -102,41 +109,57 @@ TH1D* LAPPDresponse::GetTrace(int CHnumber, int parity, double starttime, double
   tracename += "_";
   if(parity==1) tracename+="right";
   else tracename+="left";
-
+  //create said histogram
   TH1D* trace = new TH1D(tracename,tracename,numsamples,lowend,upend);
 
+  //if there are no pulses on the strip, just generate white noise
   if(_pulseCluster->GetNPulsesStrip(CHnumber)==0) {
 	for(int j=0; j<numsamples; j++){
 
-	  double mnoise = 1.0*(mrand->Rndm()-0.5);
+	  double mnoise = thenoise*(mrand->Rndm()-0.5);
 	  trace->SetBinContent(j+1, mnoise);
          }
   } else{
 
+  //if there are pulses on the strip, loop over the N pulses on that strip
 
       for(int k=0; k<(_pulseCluster->GetNPulsesStrip(CHnumber)); k++){
 
-	 int wPulse = _pulseCluster->GetPulseNum(CHnumber,k);	
+         //looks up the index number for pulse "k" on strip "CHnumber"
+      	 int wPulse = _pulseCluster->GetPulseNum(CHnumber,k);	
+         //gets the pulse with that index number
 	 LAPPDpulse* mpulse = _pulseCluster->GetPulse(wPulse);
+         //peak value of the signal on that strip
          double peakv = mpulse->Getpeakvalue();
+         //arrival time of the pulse
          double ptime = mpulse->Getpulsetime();
+         //transit time of the pulse along the strip
 	 double stime;
+         //looking at the signal to the left (parity=-1) or right (parity=1)?
 	 if(parity<0) stime = mpulse->Getlefttime();
          else stime = mpulse->Getrighttime();
 
+	 //sum the pulse arrival time with transit time on the strip to determine
+         //when the signal will arrive 
 	 double tottime = ptime + stime;
 
+         //loop over number of samples
 	 for(int j=0; j<numsamples; j++){
-
+                
+                //get the global time when each sample is acquired
 	  	double bcent = trace->GetBinCenter(j+1);
-
-	  	double mbincontent = 0.0;
+	  	double mbincontent=0.0;
 	  	double mnoise=0.0;
-		if(k==0) mnoise = _noiselevel*(mrand->Rndm()-0.5);
+                
+                //only add the noise on ONCE
+		if(k==0) mnoise = thenoise*(mrand->Rndm()-0.5);
 	 	mbincontent+=mnoise;
 
+                //if the sample time actually falls in the window for when the pulse
+                //should arrive, evaluate the pulse value at that sample point
            	if( (bcent > tottime) && (bcent< tottime+3000) ) mbincontent+=(peakv*(_templatepulse->Interpolate(bcent-tottime)));
-
+ 		
+                //add this on to the contributions to the trace from previous pulses
 		double obincontent = trace->GetBinContent(j+1);
 		trace->SetBinContent(j+1,obincontent+mbincontent);
 
@@ -155,13 +178,14 @@ int LAPPDresponse::FindStripNumber(double trans){
 
 double newtrans = trans + 101.6;
 
+// the first and last strips have a different width
 int stripnum=-1;
 if(newtrans<5.765) stripnum = 1;
 if(newtrans>197.435) stripnum = 30;
 
 double stripdouble;
 if(stripnum==-1){
-
+   // divide the 28 remaining strips into the remaining area 
   double stripdouble = 28.0*((newtrans-5.765)/(203.2 - 11.53));
   stripnum = 2 + floor(stripdouble);
 }
@@ -173,11 +197,12 @@ return stripnum;
 double LAPPDresponse::StripCoordinate(int stripnumber){
 
 double coor = -55555.;
+// the first and last strips have a different width
 if(stripnumber==1) coor = (2.31-101.6);
 if(stripnumber==30) coor = (101.6-2.31);
 
 if( stripnumber>1 && stripnumber<30 ){
-
+// remaining 28 strips have the same spacing
 coor= (5.765-101.6+3.455) + (stripnumber-2)*6.91;
 }
 
