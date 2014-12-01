9d343f70e63d609452e6f76a523f0a29c6c999b6
Matthew Wetstein <mwetstein@Matthews-MacBook-Air-3.local>
2014-11-30 23:02:02 -0600

    fully functional with maketrace function

--- include/LAPPDresponse.hh
+++ include/LAPPDresponse.hh
@@ -1,8 +1,11 @@
 #ifndef LAPPDRESPONSE_HH
 #define LAPPDRESPONSE_HH
 
+#include "LAPPDpulse.hh"
+#include "LAPPDpulseCluster.hh"
 #include "TObject.h"
 #include "TH1.h"
+#include "TRandom3.h"
 
 class LAPPDresponse : public TObject {
 
@@ -12,17 +15,18 @@ class LAPPDresponse : public TObject {
 
   ~LAPPDresponse();
 
-  void SetFreezeTime(double freezetime);
-
-  void AddPhoton(double trans, double para, double time);
-
-  TH1D* CalcSinglePhotonTrace(int CHnumber, double trans, double para, double time, int parity);
+  void AddSinglePhotonTrace(double trans, double para, double time);
 	
-  TH1D* GetTrace(int CHnumber, int parity);
+  TH1D* GetTrace(int CHnumber, int parity, double starttime, double samplesize, int numsamples);
 
+  int FindStripNumber(double trans);
 
+  double StripCoordinate(int stripnumber);
+
+  LAPPDpulseCluster* GetPulseCluster() {return _pulseCluster;}
 
  private: 
+
 
 
   //relevant to a particular event
@@ -40,10 +44,14 @@ class LAPPDresponse : public TObject {
   TH1D** StripResponse_neg;
   TH1D** StripResponse_pos;
 
+  LAPPDpulseCluster* _pulseCluster;
+
+  //randomizer
+  TRandom3* mrand;	
+
   //useful functions
   int FindNearestStrip(double trans);
   double TransStripCenter(int CHnum);
-
   
   ClassDef(LAPPDresponse,0)
 
