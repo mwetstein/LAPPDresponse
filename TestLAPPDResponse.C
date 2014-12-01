5be2f47169bf73591dfb6f13fba056cd395ca05e
Matthew Wetstein <mwetstein@Matthews-MacBook-Air-3.local>
2014-12-01 07:33:10 -0600

    improving the example

--- TestLAPPDResponse.C
+++ TestLAPPDResponse.C
@@ -23,38 +23,45 @@
 
  LAPPDresponse* mlappd = new LAPPDresponse();
  double coor = 101.6-5.766;
+ 
+ // test function to get the strip number for a given transverse coordinate (mm)	
  int sno = mlappd->FindStripNumber(coor);
  cout<<"strip number for: " <<coor<<" "<<sno<<endl;
 
+ // test function to get the transverse coordinate (mm) for a given strip number
  int stripuse=30;
  double couse = mlappd->StripCoordinate(stripuse);
  cout<<"strip number: "<<stripuse<<" stripcoordinate: "<<couse<<endl;
 
+ // add a photon hit to the LAPPDresponse class
+ // AddSinglePhotonTrace(position_transverse_tostrips(mm), position_parallel_tostrips(mm), global_time(psec))
  mlappd->AddSinglePhotonTrace(coor-20.0, 50, 1000);
- mlappd->AddSinglePhotonTrace(coor-22.0, 50, 2800);
+ // add an additional photon, slightly more towards the center
+ mlappd->AddSinglePhotonTrace(coor-22.0, -10, 2800);
 
-
+ // Get the cluster storing all of the pulses on the LAPPD
  LAPPDpulseCluster* mclust = mlappd->GetPulseCluster();
+
  for(int i=0; i<30; i++){
-  
-  cout<<"channel "<<i+1<<" number of hits: "<<mclust->GetNPulsesStrip(i+1)<<endl;	
+   
+   // loop over 30 strips, query the cluster of pulses - how many hits (if any) are on the strip
+   cout<<"channel "<<i+1<<" number of hits: "<<mclust->GetNPulsesStrip(i+1)<<endl;	
 
  }
 
 
- // TH1D* mtrace_right = mlappd->GetTrace(11, 1, 800, 100., 300);
-
-
+  // Get the trace from the right side of strip #26
+  // GetTrace(stripnumber, parity(+1=right, -1=left), start_time (psec), interval_between_samples (psec), number_of_samples)
   TH1D* mtrace_right = mlappd->GetTrace(26, 1, 800, 100., 300);
-  mtrace_right->Draw("L*");
+   mtrace_right->Draw("L*");
+  // Get the trace from the left side of strip #26
   TH1D* mtrace_left = mlappd->GetTrace(26, -1, 800, 100., 300);
   mtrace_left->SetLineColor(2);
   mtrace_left->Draw("L* SAME");
 
-
+  // Make a 2D hist of the response on the right side of 4 strips
   double highedge = mtrace_right->GetXaxis()->GetXmax();
   double lowedge = mtrace_right->GetXaxis()->GetXmin();
-
   TH2D* trace2d = new TH2D("trace2d","trace2d",4,25,29,300,lowedge,highedge);
   
   for(int i=25; i<29; i++){
@@ -68,6 +75,5 @@
 
   TCanvas* tc = new TCanvas();
   trace2d->Draw("surf3");
- // trace2d->Draw("SPEC dm(1,10) pa(2,1,1) ci(1,4,8) a(15,45,0)");
 }
 
